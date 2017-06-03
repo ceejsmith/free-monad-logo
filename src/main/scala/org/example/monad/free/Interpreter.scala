50c9de122831fcd8bf702487c6990e84017b6386
Chris Smith <chris.smith@ovoenergy.com>
2017-06-03 08:45:35 +0100

    Add new interpreter

--- src/main/scala/org/example/monad/free/Interpreter.scala
+++ src/main/scala/org/example/monad/free/Interpreter.scala
@@ -1,14 +1,27 @@
 package org.example.monad.free
 
 import cats.{Id, ~>}
+import Computations._
 
 object InterpreterId extends (Instruction ~> Id) {
-  import Computations._
+
   override def apply[A](fa: Instruction[A]): Id[A] = fa match {
     case Forward(p, length) => forward(p, length)
     case Backward(p, length) => backward(p, length)
     case RotateLeft(p, degree) => left(p, degree)
     case RotateRight(p, degree) => right(p, degree)
     case ShowPosition(p) => println(s"showing position $p")
+  }
+}
+
+object InterpreterOpt extends (Instruction ~> Option) {
+  private def nonNegative(p: Position) = if (p.x >= 0 && p.y >= 0) Some(p) else None
+
+  def apply[A](fa: Instruction[A]): Option[A] = fa match {
+    case Forward(p, length) => nonNegative(forward(p, length))
+    case Backward(p, length) => nonNegative(backward(p, length))
+    case RotateLeft(p, degree) => Some(left(p, degree))
+    case RotateRight(p, degree) => Some(right(p, degree))
+    case ShowPosition(p) => Some(println(s"showing position $p"))
   }
 }
--- src/main/scala/org/example/monad/free/Main.scala
+++ src/main/scala/org/example/monad/free/Main.scala
@@ -1,18 +1,21 @@
 package org.example.monad.free
 
+import cats.implicits._
+
 object Main {
   val program = {
     import Lifting._
     start: Position =>
       for {
         p1 <- forward(start, 10)
-        p2 <- right(p1, Degree(90))
+        p2 <- left(p1, Degree(90))
         p3 <- forward(p2, 10)
+        _ <- showPosition(p3)
       } yield p3
   }
 
   def main(args: Array[String]): Unit = {
     val startPosition = Position(0.0, 0.0, Degree(0))
-    println(program(startPosition).foldMap(InterpreterId))
+    val _ = program(startPosition).foldMap(InterpreterOpt)
   }
 }
