3abec857ed27adefe7d9bc5430a51661bb169e30
Chris Smith <chris.smith@ovoenergy.com>
2017-06-05 17:17:04 +0100

    Add logging to calculations

--- src/main/scala/org/example/monad/free/Interpreter.scala
+++ src/main/scala/org/example/monad/free/Interpreter.scala
@@ -5,10 +5,22 @@ import Computations._
 
 object InterpreterId extends (Instruction ~> Id) {
   override def apply[A](fa: Instruction[A]): Id[A] = fa match {
-    case Forward(p, length) => forward(p, length)
-    case Backward(p, length) => backward(p, length)
-    case RotateLeft(p, degree) => left(p, degree)
-    case RotateRight(p, degree) => right(p, degree)
-    case ShowPosition(p) => println(s"showing position $p")
+    case Forward(p, length) => {
+      println(s"Moving forward a distance of $length from (${p.x}, ${p.y})")
+      forward(p, length)
+    }
+    case Backward(p, length) => {
+      println(s"Moving backward a distance of $length from (${p.x}, ${p.y})")
+      backward(p, length)
+    }
+    case RotateLeft(p, degree) => {
+      println(s"Rotating left through ${degree.value} degrees on point (${p.x}, ${p.y})")
+      left(p, degree)
+    }
+    case RotateRight(p, degree) => {
+      println(s"Rotating right through ${degree.value} degrees on point (${p.x}, ${p.y})")
+      right(p, degree)
+    }
+    case ShowPosition(p) => println(s"Now at (${p.x}, ${p.y}) facing ${p.heading.value} degrees")
   }
 }
